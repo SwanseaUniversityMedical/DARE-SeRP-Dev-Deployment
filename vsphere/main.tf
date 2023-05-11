be5086960314a5d45114df6d85e28d864ff20335
Alex Lee <8878798+alee-x@users.noreply.github.com>
2023-05-11 14:04:59 +0100

    Terraform for VM creation with vSphere hypervisor (#3)

---
+++ vsphere/main.tf
@@ -0,0 +1,101 @@
+terraform {
+  required_providers {
+    vsphere = {
+      source  = "hashicorp/vsphere"
+      version = "2.4.0"
+    }
+  }
+}
+
+locals {
+  create_groups = (var.vsphere_vm_multi_cluster == true ? toset(["mgmt", "mon", "work"]) : toset(["all"]))
+  num_vms       = (var.vsphere_vm_multi_cluster == true ? range(1, var.vsphere_vm_default_number_cluster + 1) : [1])
+
+  vm_list_def = distinct(flatten([
+    for group in local.create_groups : [
+      for vmn in local.num_vms : {
+        group     = group
+        vm_ref    = vmn
+        cpu       = var.vsphere_vm_default_num_cpu
+        memory    = var.vsphere_vm_default_memory
+        os_disk   = var.vsphere_vm_default_os_disk_size
+        data_disk = var.vsphere_vm_default_data_disk_size
+      }
+    ]
+  ]))
+
+  vm_list_cr = flatten([
+    for group, group_data in var.vsphere_vm_multi_cluster_custom_resource_defs : [
+      for vmn in range(1, group_data["num_vms"] + 1) : {
+        group     = group
+        vm_ref    = vmn
+        cpu       = group_data["cpu"]
+        memory    = group_data["memory"]
+        os_disk   = group_data["os_disk"]
+        data_disk = group_data["data_disk"]
+      }
+    ]
+  ])
+
+  vm_list = (var.vsphere_vm_multi_cluster == true && var.vsphere_vm_multi_cluster_customise_resources == true ? local.vm_list_cr : local.vm_list_def)
+
+}
+
+provider "vsphere" {
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "datacenter" {
+  name = var.vsphere_datacenter
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.vsphere_datastore
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = var.vsphere_compute_cluster
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.vsphere_network
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  for_each            = { for item in local.vm_list : "${item.group}.${item.vm_ref}" => item }
+  name                = "${var.vsphere_vm_name}-${each.value.group}-${each.value.vm_ref}"
+  folder              = var.vsphere_vm_folder
+  resource_pool_id    = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id        = data.vsphere_datastore.datastore.id
+  num_cpus            = each.value.cpu
+  memory              = each.value.memory
+  guest_id            = "ubuntu64Guest"
+  sync_time_with_host = true
+
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+  disk {
+    label            = "disk0"
+    size             = each.value.os_disk
+    thin_provisioned = false
+  }
+  # disk {
+  #   label            = "disk1"
+  #   size             = each.value.data_disk
+  #   unit_number      = 1
+  #   thin_provisioned = true
+  # }
+  # need this so it can install vmware tools
+  cdrom {
+    client_device = true
+  }
+
+}
+
---
+++ vsphere/variables.tf
@@ -0,0 +1,124 @@
+variable "vsphere_user" {
+  description = "Username of VSphere account"
+  type        = string
+  sensitive   = true
+}
+
+variable "vsphere_password" {
+  description = "Password of VSphere account"
+  type        = string
+  sensitive   = true
+}
+
+variable "vsphere_server" {
+  description = "Address of VSphere hypervisor"
+  type        = string
+  sensitive   = true
+}
+
+variable "vsphere_datacenter" {
+  description = "Name of VSphere DC"
+  type        = string
+  default     = ""
+}
+
+variable "vsphere_datastore" {
+  description = "Name of VSphere DS"
+  type        = string
+  default     = ""
+}
+
+variable "vsphere_compute_cluster" {
+  description = "Name of VSphere compute cluster"
+  type        = string
+  default     = ""
+}
+
+variable "vsphere_network" {
+  description = "Name of VSphere network"
+  type        = string
+  default     = ""
+}
+
+variable "vsphere_vm_name" {
+  description = "Name of VSphere VM to create"
+  type        = string
+  default     = "TeleportTestVM"
+}
+
+variable "vsphere_vm_folder" {
+  description = "The path to the virtual machine folder in which to place the virtual machine, relative to the datacenter path "
+  type        = string
+  default     = ""
+}
+
+variable "vsphere_vm_default_num_cpu" {
+  description = "The number of CPUs to allocate to each VM."
+  type        = number
+  default     = 8
+}
+
+variable "vsphere_vm_default_memory" {
+  description = "The amount of memory, in megabytes, to allocate each VM."
+  type        = number
+  default     = 32000
+}
+
+variable "vsphere_vm_default_os_disk_size" {
+  description = "The size, in gigabytes, for the OS disk of the VM"
+  type        = number
+  default     = 70
+}
+
+variable "vsphere_vm_default_data_disk_size" {
+  description = "The size, in gigabytes, for the data disk of the VM. This disk is where the K8s storage class will write to."
+  type        = number
+  default     = 50
+}
+
+
+variable "vsphere_vm_multi_cluster" {
+  description = "Value indicating whether to create one monolithic VM in which all three clusters will run, or whether clusters will be separated out into their own VM(s)."
+  type        = bool
+  default     = false
+}
+
+variable "vsphere_vm_default_number_cluster" {
+  description = "The number of VMs to create for each of the three clusters. Only applies when vsphere_vm_multi_cluster is true."
+  type        = number
+  default     = 1
+}
+
+variable "vsphere_vm_multi_cluster_customise_resources" {
+  description = "Set to true to pass a customised resource definition for VMs in each group. Only applies when vsphere_vm_multi_cluster is true. If not set then every created VM will have resources defined in vsphere_vm_default_num_cpu, vsphere_vm_default_memory, vsphere_vm_default_os_disk_size, and vsphere_vm_default_data_disk_size."
+  type        = bool
+  default     = false
+}
+
+variable "vsphere_vm_multi_cluster_custom_resource_defs" {
+  description = "The customised resource values for VMs in each of the VM groups. Only applies when vsphere_vm_multi_cluster is true and vsphere_vm_multi_cluster_customise_resources is true."
+  type        = map(any)
+  default = {
+    "work" = {
+      "num_vms"   = 3
+      "cpu"       = 4
+      "memory"    = 8000
+      "os_disk"   = 70
+      "data_disk" = 100
+    }
+    "mon" = {
+      "num_vms"   = 1
+      "cpu"       = 8
+      "memory"    = 16000
+      "os_disk"   = 70
+      "data_disk" = 50
+    }
+    "mgmt" = {
+      "num_vms"   = 1
+      "cpu"       = 8
+      "memory"    = 16000
+      "os_disk"   = 70
+      "data_disk" = 50
+    }
+  }
+}
